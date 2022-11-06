e744b03fdbc2710d097e4855e22f3043be1df025
Poonam7505 <Poonam82872@gmail.com>
2022-11-06 14:28:38 +0530

    lazy load implemented

--- src/Component/Lazyload.js
+++ src/Component/Lazyload.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { Suspense } from 'react';
 import axios from 'axios';
 import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+// import  from './Singleproduct';/
 import '../CSS/Card.css'
 import Navbar from './Navbar';
 const Singleproduct = React.lazy(() => import("./Singleproduct"));
   const Lazyload=()=> {
+  // const navigate=useNavigate();
 
   const [data, setdata] = useState([ ]);
   const [loading, setLoading] = useState(true);
+  const [nexturl, setnexturl] = useState()
+  const [prevurl, setprevurl] = useState()
 
   const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
   const Poke=async()=>{
   setLoading(true);
   const res=await axios.get(url);
-  
+  setnexturl(res.data.next);
+  setprevurl(res.data.previous);
   get(res.data.results);
   setLoading(false);
   }
@@ -46,9 +52,22 @@ setdata(state=>{
 <div className='product'>
 
 <Navbar/>
+
 <Suspense fallback={<div>Loading</div>}>
 <Singleproduct data={data} loading={loading}/>
 			</Suspense>
+      <button className='btn'
+       onClick={()=>{
+        setUrl(prevurl);
+      }}
+      >prev</button>
+      <button  className='btn'
+      onClick={()=>{
+        setdata([ ]);
+        setUrl(nexturl);
+      }}
+      >next</button>
+     
 </div>
 </>
 
